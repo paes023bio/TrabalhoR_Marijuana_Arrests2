3fb7d1ecb18af03966c31ee5bbce71cfecd879b2
paes023bio <paes2122@outlook.com>
2023-06-22 17:18:53 -0300

    Update trabalho.R

--- trabalho.R
+++ trabalho.R
@@ -1,6 +1,9 @@
 #trabalho R
-i<- 10
-x<- length(i)
-x 
-print(c(i,x))
-paste0("O valor de i é: ", i, " Esse número tem tamanho igual a: ", x)
+# importação do conjunto de dados Marijuana_Arrests
+library(readr)
+# url<- "https://github.com/paes023bio/TrabalhoR_Marijuana_Arrests2/blob/6c7aac9946ae59fd17a903c97ac3d0759a856cb3/Marijuana_Arrests.csv"
+# db_Marijuana_Arrests<- read_csv(url)
+
+library(readr)
+Marijuana_Arrests <- read_csv("C:/Users/paes2/Desktop/disciplinas/Linguagem R/trabalho/TrabalhoR_Marijuana_Arrests/Marijuana_Arrests.csv")
+View(Marijuana_Arrests)
