50e00e2789e5693c00face6bce66e0a1102d6acb
paes023bio <paes2122@outlook.com>
2023-06-23 12:55:02 -0300

    Update trabalho.R

--- trabalho.R
+++ trabalho.R
@@ -1,9 +1,30 @@
 #trabalho R
-# importação do conjunto de dados Marijuana_Arrests
-library(readr)
-# url<- "https://github.com/paes023bio/TrabalhoR_Marijuana_Arrests2/blob/6c7aac9946ae59fd17a903c97ac3d0759a856cb3/Marijuana_Arrests.csv"
-# db_Marijuana_Arrests<- read_csv(url)
 
+#carregamento dos dados
 library(readr)
+
+# importação do conjunto de dados Marijuana_Arrests
 Marijuana_Arrests <- read_csv("C:/Users/paes2/Desktop/disciplinas/Linguagem R/trabalho/TrabalhoR_Marijuana_Arrests/Marijuana_Arrests.csv")
 View(Marijuana_Arrests)
+ url<- "https://github.com/paes023bio/TrabalhoR_Marijuana_Arrests2/blob/6c7aac9946ae59fd17a903c97ac3d0759a856cb3/Marijuana_Arrests.csv"
+db_Marijuana_Arrests<- read_csv(url)
+
+
+# Perguntas de interesse:
+# 1.
+# 2.
+# 3.
+# 4.
+# 5.
+
+
+
+
+
+
+
+
+
+
+
+
